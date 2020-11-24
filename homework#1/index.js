3ed70c0c7b53297ccd6a5af7a94d905d05681dc5
Hermann Vincent <baw.developpement@gmail.com>
2020-11-24 21:51:26 +0100

    Cleaning

--- homework#1/index.js
+++ homework#1/index.js
@@ -1,8 +1,8 @@
-const http  = require("http");
-const https  = require("https");
-const config= require("./config");
-const Router = require("./router");
-const fs      = require('fs');
+const http      = require("http");
+const https     = require("https");
+const config    = require("./config");
+const Router    = require("./router");
+const fs        = require('fs');
 
 const httpServer = http.createServer(Router);
 const httpsServer = https.createServer({
@@ -11,34 +11,15 @@ const httpsServer = https.createServer({
 }, Router);
 
 Router.get("hello", (req, res) => {
-    let payload = {
-        'welcome': 'man'
-    };
-
-    // Convert the payload to a string
-    var payloadString = JSON.stringify(payload);
-
-    // Return the response
-    // Tell the user what type of data we are sending.
     res.setHeader('Content-Type', 'application/json');
     res.writeHead(200);
-    res.end(payloadString)
+    res.end({ "message": "Hello Knight" })
 });
 
-
 Router.post("hello/you", (req, res) => {
-    let payload = {
-        'hello': 'you'
-    };
-
-    // Convert the json payload to a string
-    var payloadString = JSON.stringify(payload);
-
-    // Return the response
-    // Tell the user what type of data we are sending.
     res.setHeader('Content-Type', 'application/json');
     res.writeHead(200);
-    res.end(payloadString)
+    res.end(JSON.stringify({ 'message' : 'Welcome Sorcerer' }))
 });
 
 const { httpPort } = config;
@@ -48,6 +29,6 @@ httpServer.listen(httpPort, () => {
 
 const { httpsPort } = config;
 httpsServer.listen(httpsPort, () => {
-    console.log(`The httpServer is listening on ${httpsPort}`)
+    console.log(`The httpsServer is listening on ${httpsPort}`)
 })
 
