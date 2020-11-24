17df2af0c4064d786b604c4ec4d3d220afc7996f
Hermann Vincent <baw.developpement@gmail.com>
2020-11-24 05:03:29 +0100

    Parsing Query Strings

--- restful_api/index.js
+++ restful_api/index.js
@@ -12,6 +12,7 @@ var port = 3000;
 
 // The server should respond to all request with a string
 var server = http.createServer(function(req, res) {
+    
     // Get the URL and parse it
     // parsedUrl is an object a bunch of keys of parsed automated
     // data about the request path the url came in
@@ -22,6 +23,10 @@ var server = http.createServer(function(req, res) {
     // Use regex to get a cleaner pathname
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+
     // Get the HTTP Method
     var method = req.method.toLocaleLowerCase();
 
@@ -29,7 +34,7 @@ var server = http.createServer(function(req, res) {
     res.end('Hello World\n');
 
     // Log the request path
-    console.log(`[${method.toLocaleUpperCase()}] - ${trimmedPath}`);
+    console.log(`[${method.toLocaleUpperCase()}] / ${trimmedPath} - Query `, queryStringObject);
 });
 
 // Start the server, and hve it listen on port 3000
