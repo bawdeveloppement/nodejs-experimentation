73ff4dc1c8a7f55d0341db34071005f9c540785f
Hermann Vincent <baw.developpement@gmail.com>
2020-11-24 04:56:05 +0100

    Parsing HTTP methods

--- restful_api/index.js
+++ restful_api/index.js
@@ -12,7 +12,6 @@ var port = 3000;
 
 // The server should respond to all request with a string
 var server = http.createServer(function(req, res) {
-
     // Get the URL and parse it
     // parsedUrl is an object a bunch of keys of parsed automated
     // data about the request path the url came in
@@ -23,11 +22,14 @@ var server = http.createServer(function(req, res) {
     // Use regex to get a cleaner pathname
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the HTTP Method
+    var method = req.method.toLocaleLowerCase();
+
     // Send the response
     res.end('Hello World\n');
 
     // Log the request path
-    console.log(`Request received on path: ${trimmedPath}`);
+    console.log(`[${method.toLocaleUpperCase()}] - ${trimmedPath}`);
 });
 
 // Start the server, and hve it listen on port 3000
