bcd5a2c0f79a4910e37c77f9b5617de0a9cff602
Hermann Vincent <baw.developpement@gmail.com>
2020-11-26 00:16:17 +0100

    add some configs vars

--- restful_api/config.js
+++ restful_api/config.js
@@ -10,12 +10,14 @@ environments.staging = {
     'httpPort'  : 3000,
     'httpsPort' : 3001,
     'envName'   : 'staging',
+    'hashSecret': 'ThisIsASecret' 
 };
 
 environments.production = {
     'httpPort'  : 5000,
     'httpsPort' : 5001,
-    'envName'   : 'production'
+    'envName'   : 'production',
+    'hashSecret': 'ThisIsAnotherSecret' 
 };
 
 // Determine which environment was passed as a command-line arg
